facb8ae080ec3f856b6bce9dfb1f824c41aefb84
Pierre Fenoll <pierrefenoll@gmail.com>
2026-04-27 18:51:19 +0200

    Rename crate after https://github.com/openscad/tree-sitter-openscad/issues/9
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- crates/tree-crasher-openscad/src/main.rs
+++ crates/tree-crasher-openscad/src/main.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 
 fn main() -> Result<()> {
     tree_crasher::main(
-        tree_sitter_openscad_ng::LANGUAGE.into(),
-        tree_sitter_openscad_ng::NODE_TYPES,
+        tree_sitter_openscad::LANGUAGE.into(),
+        tree_sitter_openscad::NODE_TYPES,
     )
 }
